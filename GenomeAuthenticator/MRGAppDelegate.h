9d01ff4ed4ae39f5d91ebe7dc5d68d570b4a67d2
Maximilian Gerlach <m@px0.de>
2014-05-15 21:14:47 -0400

    Initial Commit

---
+++ GenomeAuthenticator/MRGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MRGAppDelegate.h
+//  GenomeAuthenticator
+//
+//  Created by Maximilian Gerlach on 2014-05-15.
+//  Copyright (c) 2014 Maximilian Gerlach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GenomeAuthenticatorTests/GenomeAuthenticatorTests.m
@@ -0,0 +1,34 @@
+//
+//  GenomeAuthenticatorTests.m
+//  GenomeAuthenticatorTests
+//
+//  Created by Maximilian Gerlach on 2014-05-15.
+//  Copyright (c) 2014 Maximilian Gerlach. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GenomeAuthenticatorTests : XCTestCase
+
+@end
+
+@implementation GenomeAuthenticatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
